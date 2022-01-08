a6cf140a3d5d23e95537b134a323757d545edc21
jeff <somuadinaokoli219@gmail.com>
2022-01-08 23:47:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="nav">
+    <Navbar />
+  </div>
+  <router-view />
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+  background-color: hsl(0, 0%, 98%);
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="country__header">
+    <div class="country__header__items">
+      <h1>Where in the world?</h1>
+      <span>Dark Mode</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style lang="scss">
+.country__header {
+  background-color: #fff;
+  min-height: 100px;
+  padding: 0 20px;
+  box-shadow: -1px 3px 3px -3px rgba(166, 161, 161, 1);
+  -webkit-box-shadow: -1px 3px 3px -3px rgba(166, 161, 161, 1);
+  -moz-box-shadow: -1px 3px 3px -3px rgba(166, 161, 161, 1);
+  .country__header__items {
+    display: flex;
+    align-items: center;
+    padding-top: 40px;
+    justify-content: space-between;
+    max-width: 100%;
+    h1 {
+      font-size: 17px;
+    }
+  }
+}
+</style>
